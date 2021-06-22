493826b80a5fae6b86d3f1215555e6d4b0275f79
kwonjaeeun <dino218@naver.com>
2021-06-22 10:39:58 +0900

    asd

--- src/main/java/com/koreait/facebook/common/EmailServiceImpl.java
+++ src/main/java/com/koreait/facebook/common/EmailServiceImpl.java
@@ -1,5 +1,21 @@
 package com.koreait.facebook.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class EmailServiceImpl implements EmailService{
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@baeldung.com"); //보내는 사람 이메일
+        message.setTo(to); //받는사람 이메일 주소
+        message.setSubject(subject); //제목
+        message.setText(text); //내용
+        mailSender.send(message);
+    }
 }
