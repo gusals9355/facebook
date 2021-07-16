b5d7b225e4228b700b9be3bec94c5e5795f0cf6c
gusals9355 <gusals9355@naver.com>
2021-07-16 23:45:54 +0900

    0716

---
+++ src/main/java/com/koreait/facebook/security/model/CustomUserPrincipal.java
@@ -0,0 +1,69 @@
+package com.koreait.facebook.security.model;
+
+import com.koreait.facebook.user.model.UserEntity;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class CustomUserPrincipal implements UserDetails, OAuth2User {
+
+    @Getter private UserEntity user;
+    private Map<String, Object> attributes;
+
+    public CustomUserPrincipal(UserEntity user) {
+        this.user = user;
+    }
+    public CustomUserPrincipal(UserEntity user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPw();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() { return String.valueOf(user.getIuser()); }
+}
